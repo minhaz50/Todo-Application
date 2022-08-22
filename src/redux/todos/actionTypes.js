8d2c6a6c7d89a55b2fc2910b9f0f9cebd7c5a0ec
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-08-22 23:38:55 +0600

    first commit

---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADDED = 'todos/added';
+export const TOGGLED = 'todos/toggled';
+export const COLORSELECTED = 'todos/colorselected';
+export const CLEARCOMPLETED = 'todos/clearcompleted';
+export const ALLCOMPLETED = 'todos/allcompleted';
+export const DELETED = 'todos/deleted';
